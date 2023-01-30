3779c597dd5265c96d5ff61b4d0bec51eadaa68f
Rossiny Longo <rossiny.longo@heig-vd.ch>
2023-01-30 20:07:17 +0100

    fonction de generation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-class Open {
-  constructor() {
-    this.open = document.querySelector(".open");
+function generateColorPalette(hexCode, interval) {
+  const hslArray = [];
+  const color = allColors[hexCode].toHsl();
+
+  for (let i = 0; i <= 100; i += interval) {
+    hslArray.push({
+      h: color.h,
+      s: color.s,
+      l: i,
+    });
   }
+  return hslArray;
 }
 
-class Close {
-  constructor() {
-    this.close = document.querySelector(".close");
-  }
-}
+let hexCode = "FF0000";
+let interval = 10;
+let colorPalette = generateColorPalette(hexCode, interval);
+console.log(colorPalette);
