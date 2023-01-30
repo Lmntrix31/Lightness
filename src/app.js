27cff872369c045af07b9e10d6ca489a94159669
Rossiny Longo <rossiny.longo@heig-vd.ch>
2023-01-30 20:18:58 +0100

    Generer la palette

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
-function generateColorPalette(hexCode, interval) {
-  const hslArray = [];
-  const color = allColors[hexCode].toHsl();
+// modules/utils.js
+import { generatePalette } from "./modules/utils";
+import * as convert from "color-convert";
+export const generatePalette = (hex) => {
+  // crée un tableau vide.
+  const colors = [];
 
-  for (let i = 0; i <= 100; i += interval) {
-    hslArray.push({
-      h: color.h,
-      s: color.s,
-      l: i,
-    });
+  // converti le hex d'entrée en tableau hsl. Ne récupérer que les
+  // deux premières valeurs.
+  const [h, s] = convert.hex.hsl(hex);
+
+  // Itère entre 0 et 100 par intervalle de 10
+  for (let i = 0; i <= 100; i += 10) {
+    // À chaque itération, pousse un nouveau tableau dans colors contenant
+    // la teinte (fixe), la saturation (fixe) et la luminosité déterminée par
+    // l'index de la boucle.
+    colors.push([h, s, i]);
   }
-  return hslArray;
-}
 
-let hexCode = "FF0000";
-let interval = 10;
-let colorPalette = generateColorPalette(hexCode, interval);
-console.log(colorPalette);
+  // Retourne le tableau.
+  return colors;
+};
